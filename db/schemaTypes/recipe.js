2b02d753b8a53960464c3f38ff0defdb65603775
MalieJobe <maliejobe@outlook.de>
2024-07-04 10:33:04 +0200

    Reflect recipe data sctructure

--- db/schemaTypes/recipe.js
+++ db/schemaTypes/recipe.js
@@ -1,8 +1,8 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'post',
-  title: 'Post',
+  name: 'recipe',
+  title: 'Recipe',
   type: 'document',
   fields: [
     defineField({
